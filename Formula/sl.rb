a1ee0abe1143df3e1107531c692b40a12f2c0620
Mike Wallio <michael.wallio@outlook.com>
2024-11-24 14:58:10 -0500

    Remove auto generated comments and update links (#1)
    * Remove auto generated comments and update links
    
    * Change license to match sl core cask and update description
    
    * Remove self conflict

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -1,19 +1,16 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sl < Formula
-  desc "SL(1): Cure your bad habit of mistyping"
-  homepage ""
+  desc "Prints a steam locomotive if you type sl instead of ls"
+  homepage "https://github.com/scaryrawr/sl"
   url "https://github.com/scaryrawr/sl/archive/refs/tags/5.06.1.tar.gz"
   sha256 "62a01863082ab8d660785374ee98e55d4e4199fe6578911e0225cd2a401b2a8e"
-  license "NOASSERTION"
+  license "MIT"
+  head "https://github.com/scaryrawr/sl.git", branch: "main"
 
   depends_on "cmake" => :build
 
   uses_from_macos "ncurses"
 
   conflicts_with "sapling", because: "both install `sl` binaries"
-  conflicts_with "sl", because: "both install `sl` binaries"
 
   def install
     system "cmake", "-S", ".", "-B", "build", *std_cmake_args
