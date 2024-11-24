78cd5ce1f469a51bd02bab7dca1bddebc174cc9f
Mike Wallio <michael.wallio@outlook.com>
2024-11-24 10:35:11 -0500

    Remove auto generated comments and update links

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -1,12 +1,10 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Sl < Formula
   desc "SL(1): Cure your bad habit of mistyping"
-  homepage ""
+  homepage "https://github.com/scaryrawr/sl"
   url "https://github.com/scaryrawr/sl/archive/refs/tags/5.06.1.tar.gz"
   sha256 "62a01863082ab8d660785374ee98e55d4e4199fe6578911e0225cd2a401b2a8e"
-  license "NOASSERTION"
+  license "https://github.com/scaryrawr/sl/blob/main/LICENSE"
+  head "https://github.com/scaryrawr/sl.git", branch: "main"
 
   depends_on "cmake" => :build
 
