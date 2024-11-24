a7aa1293d3b9b5412078d4dcabdc5030ee989ae1
Mike Wallio <michael.wallio@outlook.com>
2024-11-24 10:27:43 -0500

    Add scaryrawr/sl formula

---
+++ Formula/sl.rb
@@ -0,0 +1,27 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Sl < Formula
+  desc "SL(1): Cure your bad habit of mistyping"
+  homepage ""
+  url "https://github.com/scaryrawr/sl/archive/refs/tags/5.06.1.tar.gz"
+  sha256 "62a01863082ab8d660785374ee98e55d4e4199fe6578911e0225cd2a401b2a8e"
+  license "NOASSERTION"
+
+  depends_on "cmake" => :build
+
+  uses_from_macos "ncurses"
+
+  conflicts_with "sapling", because: "both install `sl` binaries"
+  conflicts_with "sl", because: "both install `sl` binaries"
+
+  def install
+    system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    system bin/"sl", "-c"
+  end
+end
