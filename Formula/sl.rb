d7731c3c34f3678f001e7915efa31db41f326689
Mike Wallio <michael.wallio@outlook.com>
2024-12-13 12:37:56 -0500

    Update to 6.0.0

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -1,35 +1,24 @@
 class Sl < Formula
   desc "Prints a steam locomotive if you type sl instead of ls"
   homepage "https://github.com/scaryrawr/sl"
-  url "https://github.com/scaryrawr/sl/archive/refs/tags/sl-5.07-1.tar.gz"
-  sha256 "c9c416b14adbc68e5c9cb6e1328144a2fd2f6c32fad202bc86a46a37e23cab5a"
+  url "https://github.com/scaryrawr/sl/archive/refs/tags/sl-6.0.0-1.tar.gz"
+  sha256 "4fa140458a8e5b9aff6dab24248f9de57bb739d42ad6d695de07d56eb2806db6"
   license "MIT"
   head "https://github.com/scaryrawr/sl.git", branch: "main"
 
   depends_on "cmake" => :build
-  depends_on "pkgconf" => :build
-  depends_on "vcpkg" => :build
-
-  depends_on "ncurses"
+  depends_on "rust" => :build
 
   conflicts_with "sapling", because: "both install `sl` binaries"
 
   def install
-    system "git", "clone", "https://github.com/microsoft/vcpkg"
-    system(
-      "cmake",
-      "-S", ".",
-      "-B", "build",
-      "-D", "CMAKE_TOOLCHAIN_FILE=#{buildpath}/vcpkg/scripts/buildsystems/vcpkg.cmake",
-      *std_cmake_args
-    )
-    system "cmake", "--build", "build"
-    system "cmake", "--install", "build"
+    system "cargo", "install", *std_cargo_args
+    man1.install "sl.1"
   end
 
   test do
     system bin/"sl", "-c"
     system bin/"sl", "-h"
-    system bin/"sl", "-v"
+    system bin/"sl", "-V"
   end
 end
