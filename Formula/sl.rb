8fd688642aba73ba00d465df28beecb218446f04
Mike Wallio <michael.wallio@outlook.com>
2024-12-17 21:51:56 -0500

    Update to 6.03 (#8)

--- Formula/sl.rb
+++ Formula/sl.rb
@@ -1,8 +1,8 @@
 class Sl < Formula
   desc "Prints a steam locomotive if you type sl instead of ls"
   homepage "https://github.com/scaryrawr/sl"
-  url "https://github.com/scaryrawr/sl/archive/refs/tags/sl-6.02-1.tar.gz"
-  sha256 "bce2e80a40e09582da0b17850e0f5ed927b6cdb070ac6c31234a443a616a95ad"
+  url "https://github.com/scaryrawr/sl/archive/refs/tags/sl-6.03-1.tar.gz"
+  sha256 "c0623f01f7425b4cff47154ba1fb42115ae96535b8df2239c0d177646c121e01"
   license "MIT"
   head "https://github.com/scaryrawr/sl.git", branch: "main"
 
