d7e89d69154cba2a384eaa6ee0937def6d1e08d2
GitHub Action <action@github.com>
2025-07-10 18:38:12 +0000

    chore: update formula versions

--- Formula/opencode.rb
+++ Formula/opencode.rb
@@ -1,27 +1,27 @@
 class Opencode < Formula
   desc "AI coding agent built for the terminal"
   homepage "https://github.com/scaryrawr/opencode"
-  version "0.2.16"
+  version "0.2.23"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.16/opencode-darwin-x64.tar.gz"
-      sha256 "17122a5ec422a412e8734900b82572c598ab079156df05c77fee9c2f1a601bb6"
+      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.23/opencode-darwin-x64.tar.gz"
+      sha256 "180c8bb997dd06adbfff9794a1ac8c4b201bfac13bd962914ea0dbad4bab845e"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.16/opencode-darwin-arm64.tar.gz"
-      sha256 "683124029e8334ec3943c193c162dc2bf0eb742df784508c0780024bfdb3467f"
+      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.23/opencode-darwin-arm64.tar.gz"
+      sha256 "93902a56e8438adbd003abf6cbd543bf2782e40d69c49ee54676b85ec5ce42e2"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.16/opencode-linux-x64.tar.gz"
-      sha256 "d50d01d0a7f47ade2b564dd2316b5397e989ffac21dfaaf9808b9c4f5f032f6b"
+      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.23/opencode-linux-x64.tar.gz"
+      sha256 "59439f48eaf043755c6c248fa53239a6aac8964db4580649dca3cff966d18915"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.16/opencode-linux-arm64.tar.gz"
-      sha256 "fdc4b985fde1ae9a853adf5b790db238f95141b6aeb4286ea2eadf83335cb4fe"
+      url "https://github.com/scaryrawr/opencode/releases/download/r0.2.23/opencode-linux-arm64.tar.gz"
+      sha256 "b96627ab1581e84adca59afa3bc1815af4907424b37f36dc6e8cde08a7e8a78f"
     end
   end
 
