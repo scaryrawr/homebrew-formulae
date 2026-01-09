9336dd4b63eb958ba1061ddcfbdf535fd2d7f267
GitHub Action <action@github.com>
2026-01-09 06:12:45 +0000

    chore: update formula versions

--- Formula/codex.rb
+++ Formula/codex.rb
@@ -1,9 +1,9 @@
 class Codex < Formula
   desc "Lightweight coding agent that runs in your terminal"
   homepage "https://github.com/scaryrawr/codex"
-  url "https://github.com/scaryrawr/codex/archive/5e7241b9de604cc6f4a46c08f93ad1592c2e3fcb.tar.gz"
-  version "0.80.0-alpha.3+5e7241b"
-  sha256 "a875a547882722faeca64c74e438eb0de38fcf50fc4dbbfdb9f13da69c9fc5e9"
+  url "https://github.com/scaryrawr/codex/archive/f24c8e3f08958502304ae97ac6e8b1eb67d50885.tar.gz"
+  version "0.80.0-alpha.5+f24c8e3"
+  sha256 "78796e391168f4a1ecd0dac6856b35e76ff9a913cbc47f9075da528865751ea2"
   license "MIT"
 
   depends_on "rust" => :build
