f3afedefb86dc6f5d242288a43c5b7ef03c2c774
GitHub Action <action@github.com>
2026-02-17 06:28:32 +0000

    chore: update formula versions

--- Formula/navigit.rb
+++ Formula/navigit.rb
@@ -1,25 +1,25 @@
 class Navigit < Formula
   desc "Context-aware Git tool dispatcher"
   homepage "https://github.com/scaryrawr/navigit"
-  version "0.0.1"
+  version "0.0.2"
   license :cannot_represent
   head "https://github.com/scaryrawr/navigit.git", branch: "main"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/scaryrawr/navigit/releases/download/v0.0.1/navigit-v0.0.1-macos-arm64.tar.gz"
-      sha256 "f11ed7166246d6a329a7893d5f9122b6d7615aede4b823d0f0803d205069a4e4"
+      url "https://github.com/scaryrawr/navigit/releases/download/v0.0.2/navigit-v0.0.2-macos-arm64.tar.gz"
+      sha256 "51eb0d9dc79b7f3c10ced4249284edfdfcd80f13e7db53980fc2d595f22ea1ac"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel? && Hardware::CPU.is_64_bit?
-      url "https://github.com/scaryrawr/navigit/releases/download/v0.0.1/navigit-v0.0.1-linux-x64.tar.gz"
-      sha256 "15a4a8c60465ed40266651a811d52d298418b36bf2f0a6e407810960675dfe9d"
+      url "https://github.com/scaryrawr/navigit/releases/download/v0.0.2/navigit-v0.0.2-linux-x64.tar.gz"
+      sha256 "2b330fc834ccfb48243017450a752888262c9bde9b9299c55eaf3a86d446c982"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/scaryrawr/navigit/releases/download/v0.0.1/navigit-v0.0.1-linux-arm64.tar.gz"
-      sha256 "ce00a1d3d590e7f6accf26f652e708480269d986d64ff4d8496ee097fcf7ac87"
+      url "https://github.com/scaryrawr/navigit/releases/download/v0.0.2/navigit-v0.0.2-linux-arm64.tar.gz"
+      sha256 "9845d8582b13cc83080474bb04e5a9fae824eafef8a91c17578351b3bab13b2a"
     end
   end
 
