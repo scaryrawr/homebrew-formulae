e2dcafcae5a5d91b2908762e13e7e1fd87e8b44b
GitHub Action <action@github.com>
2026-02-20 15:28:09 +0000

    chore: update formula versions

--- Formula/claudio.rb
+++ Formula/claudio.rb
@@ -1,8 +1,8 @@
 class Claudio < Formula
   desc "LM Studio wrapper around Claude Code"
   homepage "https://github.com/scaryrawr/claudio"
-  url "https://github.com/scaryrawr/claudio/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "87bfa926d31305600c5901ccfc96e0af8e11bdcbd58a1e9e1efd795c2e9a36ee"
+  url "https://github.com/scaryrawr/claudio/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "eacb8339181a1d9c3c13d9b7a048eff3e570b879ba0a7132641217202ffd58ef"
   license "MIT"
   head "https://github.com/scaryrawr/claudio.git", branch: "main"
 
